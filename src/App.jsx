ce4cc0e94d785059ba9d66a12d14021498f26ce2
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2023-04-10 13:06:36 +0600

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet } from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Navbar></Navbar>
+      <Outlet />
     </div>
   )
 }
